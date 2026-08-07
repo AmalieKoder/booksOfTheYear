1afd3c5e074c0cafa5fcc23376e1cdcf4af5cdd6
AmalieKoder <amlie.koder@gmail.com>
2026-08-07 10:03:08 +0200

    index

---
+++ 2025/script.js
@@ -0,0 +1,233 @@
+const imageHover = document.querySelector('.imgHover1');
+const imageHover2 = document.querySelector('.imgHover2')
+const imageHover3 = document.querySelector('.imgHover3')
+const imageHover4 = document.querySelector('.imgHover4')
+const imageHover5 = document.querySelector('.imgHover5')
+const imageHover6 = document.querySelector('.imgHover6')
+const imageHover7 = document.querySelector('.imgHover7')
+const imageHover8 = document.querySelector('.imgHover8')
+const imageHover9 = document.querySelector('.imgHover9')
+const imageHover10 = document.querySelector('.imgHover10')
+const imageHover11 = document.querySelector('.imgHover11')
+const imageHover12 = document.querySelector('.imgHover12')
+const imageHover13 = document.querySelector('.imgHover13')
+const imageHover14 = document.querySelector('.imgHover14')
+const imageHover15 = document.querySelector('.imgHover15')
+const imageHover16 = document.querySelector('.imgHover16')
+const imageHover17 = document.querySelector('.imgHover17')
+const imageHover18 = document.querySelector('.imgHover18')
+const imageHover19 = document.querySelector('.imgHover19')
+const imageHover20 = document.querySelector('.imgHover20')
+const imageHover21 = document.querySelector('.imgHover21')
+const imageHover22 = document.querySelector('.imgHover22')
+const imageHover23 = document.querySelector('.imgHover23')
+const imageHover24 = document.querySelector('.imgHover24')
+const imageHover25 = document.querySelector('.imgHover25')
+const imageHover26 = document.querySelector('.imgHover26')
+const imageHover27 = document.querySelector('.imgHover27')
+const body = document.body;
+
+imageHover.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive');
+});
+
+imageHover.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive')
+})
+
+imageHover2.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive2')
+})
+
+imageHover2.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive2')
+})
+
+imageHover3.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive3')
+})
+
+imageHover3.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive3')
+})
+
+imageHover4.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive4')
+})
+
+imageHover4.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive4')
+})
+
+imageHover5.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive5')
+})
+
+imageHover5.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive5')
+})
+
+imageHover6.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive6')
+})
+
+imageHover6.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive6')
+})
+
+imageHover7.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive7')
+})
+
+imageHover7.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive7')
+})
+
+imageHover8.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive8')
+})
+
+imageHover8.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive8')
+})
+
+imageHover9.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive9')
+})
+
+imageHover9.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive9')
+})
+
+imageHover10.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive10')
+})
+
+imageHover10.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive10')
+})
+
+imageHover11.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive11')
+})
+
+imageHover11.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive11')
+})
+
+imageHover12.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive12')
+})
+
+imageHover12.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive12')
+})
+
+imageHover13.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive13')
+})
+
+imageHover13.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive13')
+})
+
+imageHover14.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive14')
+})
+
+imageHover14.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive14')
+})
+
+imageHover15.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive15')
+})
+
+imageHover15.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive15')
+})
+
+imageHover16.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive16')
+})
+
+imageHover16.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive16')
+})
+imageHover17.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive17')
+})
+
+imageHover17.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive17')
+})
+imageHover18.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive18')
+})
+
+imageHover18.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive18')
+})
+imageHover19.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive19')
+})
+
+imageHover19.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive19')
+})
+imageHover20.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive20')
+})
+
+imageHover20.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive20')
+})
+imageHover21.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive21')
+})
+
+imageHover21.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive21')
+})
+imageHover22.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive22')
+})
+
+imageHover22.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive22')
+})
+imageHover23.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive23')
+})
+
+imageHover23.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive23')
+})
+imageHover24.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive24')
+})
+
+imageHover24.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive24')
+})
+imageHover25.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive25')
+})
+
+imageHover25.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive25')
+})
+imageHover26.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive26')
+})
+
+imageHover26.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive26')
+})
+imageHover27.addEventListener('mouseover', () => {
+    body.classList.add('hoverActive27')
+})
+
+imageHover27.addEventListener('mouseout', () => {
+    body.classList.remove('hoverActive27')
+})
